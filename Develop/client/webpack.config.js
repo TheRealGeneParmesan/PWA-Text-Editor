c40bfc0bd5993a4ef6237b4f1f22d0afc1cd6624
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-01 12:04:09 -0700

    added babel-loader in order to use ES6

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -43,12 +43,20 @@ module.exports = () => {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
+
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
 
-        },
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
 
+            },
+          },
+        },
       ],
     },
   };
