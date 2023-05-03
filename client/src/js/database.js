7487e4a93e62726116fc603f788df48e295a1ebd
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-03 11:34:29 -0700

    was missing navigate preCache

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,7 +29,7 @@ export const putDb = async (content) => {
   const request = store.put({ id: 1, value: content });
 
   const result = await request;
-  console.log('🚀 - data saved to the database', result.value);
+  console.log('🚀 - data saved to the database', result);
 }
 
 
