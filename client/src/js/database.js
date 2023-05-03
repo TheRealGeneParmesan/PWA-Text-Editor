881faea76d04a7554acc9f64d338bd71f53fe0c2
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-03 11:07:10 -0700

    moved out of develop folder & fixed put and get routes for index.db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,17 +23,16 @@ const initdb = async () => {
 export const putDb = async (content) => {
   console.log('PUT to the DB');
 
-  const jateDb = await initdb();
+  const jateDb = await openDB('jate', 1);
 
   const tx = jateDb.transaction('jate', 'readwrite');
 
   const store = tx.objectStore('jate');
 
-  const request = store.put({ content });
+  const request = store.put({ id: 1, value: content });
 
   const result = await request;
   console.log('🚀 - data saved to the database', result);
-  return result;
 }
 
 
@@ -49,10 +48,10 @@ export const getDb = async () => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.getAll();
+  const request = store.get(1);
 
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result.value);
+  return result?.value;
 };
 
